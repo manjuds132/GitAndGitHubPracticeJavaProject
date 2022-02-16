fe8825956a7640a9cfc422896ed2e14d95a8c0f1
Manju Devi <manju.devi2@q2labsolutions.com>
2022-02-16 23:11:00 +0530

    Addition file updated and add one more file - Multiplication

--- src/com/test/Addition.java
+++ src/com/test/Addition.java
@@ -3,7 +3,7 @@ package com.test;
 public class Addition {
 	
 	public static void display() {
-		System.out.println("Addition Class method...");
+		System.out.println("Addition Class < display method...");
 	}
 	
 	public static void main(String []  arg) {
